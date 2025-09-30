d2d20814a15827cc73f882c4cab1180afe85b4c2
Your Name <you@example.com>
2025-09-30 13:06:18 +0700

    feat: add Spatie permission config and migrations

---
+++ config/permission.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'models' => [
+        'permission' => Spatie\Permission\Models\Permission::class,
+        'role' => Spatie\Permission\Models\Role::class,
+    ],
+
+    'table_names' => [
+        'roles' => 'roles',
+        'permissions' => 'permissions',
+        'model_has_permissions' => 'model_has_permissions',
+        'model_has_roles' => 'model_has_roles',
+        'role_has_permissions' => 'role_has_permissions',
+    ],
+
+    'column_names' => [
+        'model_morph_key' => 'model_id',
+    ],
+
+    'cache' => [
+        'expiration_time' => 60 * 24,
+        'key' => 'spatie.permission.cache',
+        'model_key' => 'name',
+    ],
+];
